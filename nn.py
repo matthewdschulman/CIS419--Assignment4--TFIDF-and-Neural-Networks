ff30f5468f1d320757730f9c42c99773eada6803
Matt Schulman <matthewdschulman@gmail.com>
2015-11-08 19:14:37 -0500

    initial commit

---
+++ nn.py
@@ -0,0 +1,46 @@
+'''
+    TEMPLATE FOR MACHINE LEARNING HOMEWORK
+    AUTHOR Eric Eaton
+'''
+
+import numpy as np
+
+class NeuralNet:
+
+    def __init__(self, layers, epsilon=0.12, learningRate, numEpochs=100):
+        '''
+        Constructor
+        Arguments:
+        	layers - a numpy array of L-2 integers (L is # layers in the network)
+        	epsilon - one half the interval around zero for setting the initial weights
+        	learningRate - the learning rate for backpropagation
+        	numEpochs - the number of epochs to run during training
+        '''
+      
+
+    def fit(self, X, y):
+        '''
+        Trains the model
+        Arguments:
+            X is a n-by-d numpy array
+            y is an n-dimensional numpy array
+        '''
+
+
+    def predict(self, X):
+        '''
+        Used the model to predict values for each instance in X
+        Arguments:
+            X is a n-by-d numpy array
+        Returns:
+            an n-dimensional numpy array of the predictions
+        '''
+    
+    
+    def visualizeHiddenNodes(self, filename):
+        '''
+        CIS 519 ONLY - outputs a visualization of the hidden layers
+        Arguments:
+            filename - the filename to store the image
+        '''
+        
