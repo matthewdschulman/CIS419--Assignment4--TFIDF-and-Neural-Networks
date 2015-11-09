ff7f05aeb6d5992ff60b83372a4ca8ef660d7a35
Matt Schulman <matthewdschulman@gmail.com>
2015-11-09 00:11:06 -0500

    get initial accuracy to show up as .77...

--- textShowdown20News.py
+++ textShowdown20News.py
@@ -3,6 +3,8 @@
     AUTHOR Matt Schulman
 '''
 
+import numpy as np
+
 from sklearn.datasets import fetch_20newsgroups
 from pprint import pprint
 from sklearn.pipeline import Pipeline
@@ -14,10 +16,14 @@ from sklearn.naive_bayes import MultinomialNB
 newsgroups_train = fetch_20newsgroups(subset='train')
 
 # import the 20 newsgroups data set (testing)
-newsgroups_test = fetch_20newsgroups(subset='train')
+newsgroups_test = fetch_20newsgroups(subset='test')
 
 text_clf = Pipeline([('vect', CountVectorizer()),
 		     ('tfidf', TfidfTransformer()),
 		     ('clf', MultinomialNB()),
 		   ])
 text_clf = text_clf.fit(newsgroups_train.data, newsgroups_train.target)
+
+docs_test = newsgroups_test.data
+predicted = text_clf.predict(docs_test)
+print(np.mean(predicted == newsgroups_test.target))
