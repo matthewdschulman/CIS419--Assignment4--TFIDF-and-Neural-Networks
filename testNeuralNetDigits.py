3cab834359c658e020823da2d1502fb658f27aaa
Matt Schulman <matthewdschulman@gmail.com>
2015-11-10 07:56:12 -0500

    add basic file for testing neural networks

---
+++ testNeuralNetDigits.py
@@ -0,0 +1,11 @@
+# learning rate parameters to be trained by hand
+num_of_epochs = 100
+regularization_parameter = 0.001
+num_of_hidden_layers = 1
+nodes_in_hidden_layer = 100
+
+# train neural network on digits data
+
+# find the training accuracy
+
+# report the training accuracy
