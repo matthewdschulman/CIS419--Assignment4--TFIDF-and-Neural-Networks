0c8d70fb2c232da2596ef2727f6f59f523f47fd8
Matt Schulman <matthewdschulman@gmail.com>
2015-11-10 14:05:56 -0500

    get initial uniform epsilon weights with correct theta sizes

--- nn.py
+++ nn.py
@@ -20,6 +20,8 @@ class NeuralNet:
 	self.epsilon = epsilon
 	self.learningRate = learningRate
 	self.numEpochs = numEpochs
+	self.neuralNet = None
+	self.weightThetas = None
       
 
     def fit(self, X, y):
@@ -29,6 +31,48 @@ class NeuralNet:
             X is a n-by-d numpy array
             y is an n-dimensional numpy array
         '''
+	n,d = X.shape
+
+	# create all of the weight matrices theta(1),...,theta(L-1)
+	weightThetas = dict()
+	
+	# initialize all of the weights to be uniformly chosen from [-epsilon, epsilon]
+	# note that if network has s_j units in layer j and s_j+1 units in layer j + 1,
+	# theta_j has dimension s_j+1 x (s_j + 1)
+	for i in range(len(self.layers) + 1):
+	    # find the dimensions for the current theta matrix
+	    d1 = -1
+	    d2 = -1
+	    # check if first theta
+	    if (i == 0):
+	        d2 = 2 # TODO: UPDATE THIS PLACEHOLDER
+		d1 = self.layers[i]
+	    
+	    # check if last theta
+	    if (i == len(self.layers)):
+		 d2 = self.layers[i-1]
+		 d1 = 1 # TODO: UPDATE THIS PLACEHOLDER
+
+	    # update d1 and d2 if not the first or last theta
+	    if (d1 == -1):
+            	d1 = self.layers[i + 1]
+
+	    if (d2 == -1):
+	        d2 = self.layers[i] + 1
+
+	    # create current theta matrix with weights uniformly chosen from [-epsilon, epsilon]
+	    weightThetas[i+1] = (self.epsilon * 2) * np.random.random_sample((d1, d2)) - self.epsilon
+	
+	
+	print weightThetas
+
+	# unroll the weight matrices theta(1),...,theta(L-1) into a single long vector, theta,
+	# that contains all parameters for the neural net
+	# theta = np.empty(len(self.layers) + 1)
+	# for i in (1, len(self.layers) + 1):
+	# 	theta[i] = weightThetas[i]
+
+	# print theta
 
 
     def predict(self, X):
--- testNeuralNetDigits.py
+++ testNeuralNetDigits.py
@@ -20,9 +20,7 @@ numEpochs = 100
 learningRate = 0.3
 epsilon = 0.12
 regularization_parameter = 0.001
-num_of_hidden_layers = 1
-nodes_in_hidden_layer = 100
-layers = []
+nodes_in_hidden_layers = [25]
 
 # load the data
 filenameX = 'data/digitsX.dat'
@@ -32,9 +30,10 @@ dataY = loadtxt(filenameY, delimiter=',')
 n,d = dataX.shape
 
 # create NeuralNet class
-modelNN = NeuralNet(layers, epsilon, learningRate, numEpochs)
+modelNN = NeuralNet(nodes_in_hidden_layers, epsilon, learningRate, numEpochs)
 
 # train neural network on digits data
+modelNN.fit(dataX, dataY)
 
 # find the training accuracy
 
